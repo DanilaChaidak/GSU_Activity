e503110f1176dc14adef1fb8945e82c3eaec3224
Danila <danchajdak@gmail.com>
2022-11-04 00:40:05 +0300

    Initial commit

---
+++ src/main/java/com/itproject/gsu_activity/GsuActivityApplication.java
@@ -0,0 +1,13 @@
+package com.itproject.gsu_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsuActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsuActivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itproject/gsu_activity/GsuActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproject.gsu_activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsuActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
